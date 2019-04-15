62f705ab71fb5071335c9d4c1b100259a3544d91
enahe <enahe@udel.edu>
2019-04-15 14:38:39 -0700

    Added comments for client and server

--- client.c
+++ client.c
@@ -1,3 +1,8 @@
+
+//client.c: contains all the code necessary to transfer files from client to server. Creates a socket for the client, connects it to the server, and asks the server for files to transfer. 
+
+
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -11,22 +16,21 @@
 int main () {
    int clientSocket;
    int connected;
-   ssize_t length;
+   ssize_t length; //length of file to be transfered. 
    struct sockaddr_in serverAddress;
    char buffer[BUFSIZ];
-   char serverIP[25];
-   char fileName[100];
-   char continueSize[1];
+   char serverIP[25]; // string of server's ip address
+   char fileName[100]; // string of filename to copy
+   char continueSize[1]; // string asking user if they want to continue
    int fileSize; 
    int continueSend = 1;
-   int convertedSend; 
    FILE *recievedFile;
    int remainingData = 0;
    struct hostent *hostIP;
    printf("Hello. Please enter the ip address of the server you would like to connect to.\n");
    scanf("%s", serverIP);
-   hostIP = gethostbyname(serverIP); 
-   clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); 
+   hostIP = gethostbyname(serverIP); //get the ip address of the server 
+   clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); //create client socket
    if (clientSocket != -1) {
        printf("Client socket has been created\n");
    }
@@ -34,11 +38,11 @@ int main () {
        printf("Client socket has not been created. Try again\n"); 
        exit(EXIT_FAILURE);
    }
-   memset(&serverAddress, 0, sizeof(serverAddress));
+   memset(&serverAddress, 0, sizeof(serverAddress)); //0 out serverAddress 
    serverAddress.sin_family = AF_INET;
    serverAddress.sin_port = htons(2500);
-   serverAddress.sin_addr= *((struct in_addr*)hostIP->h_addr);
-   connected = connect(clientSocket, (struct sockaddr*) &serverAddress, sizeof(serverAddress));
+   serverAddress.sin_addr= *((struct in_addr*)hostIP->h_addr); //create the server's address
+   connected = connect(clientSocket, (struct sockaddr*) &serverAddress, sizeof(serverAddress)); //connect the client to the server
    if (connected != -1) {
        printf("Connected to server successfully!\n");
    }
@@ -46,17 +50,17 @@ int main () {
        printf("Did not connect to server. Please try again.\n");
        exit(EXIT_FAILURE); 
    }
-   while(continueSend) {
+   while(continueSend) { //continue to ask for more files, as long as the user wants more
    printf("Please enter the file name you would like to retrieve from the server. Please also keep the file name under 99 characters.\n");
    scanf("%s", fileName); 
-   if (send(clientSocket, fileName, sizeof(fileName), 0) != -1) {
+   if (send(clientSocket, fileName, sizeof(fileName), 0) != -1) { //send the filename that you would like to copy to server
        printf("Successfully sent filename to server. Awating response.\n");
    }
    else {
        printf("File name was not sent to server. Please try again\n"); 
        exit(EXIT_FAILURE);
    }
-   if (recv(clientSocket, buffer, BUFSIZ, 0) != -1) {
+   if (recv(clientSocket, buffer, BUFSIZ, 0) != -1) { //get size of file from server
        printf("Successfully recieved file size back from server. Beginning transmission\n");
    }
    else {
@@ -66,7 +70,7 @@ int main () {
    fileSize = atoi(buffer);
        printf("The client recieved a file size of %d\n", fileSize);
    
-   recievedFile = fopen(fileName, "w"); 
+   recievedFile = fopen(fileName, "w"); //open an empty file on the client with the same name as the one on the server
    if (recievedFile == NULL) {
        printf("Failed to open the file requested. Please try again\n");
        exit(EXIT_FAILURE);
@@ -74,18 +78,18 @@ int main () {
    remainingData = fileSize; 
    printf("The client recieved a file size of %d\n", remainingData);
    memset(buffer, 0, sizeof(buffer));
-   while ((remainingData > 0) && ((length = read(clientSocket, buffer, BUFSIZ)) > 0)) {
+   while ((remainingData > 0) && ((length = read(clientSocket, buffer, BUFSIZ)) > 0)) { //while the server continues to send more data
         
-	fprintf(recievedFile, "%s", buffer);
-        remainingData -= length;
+	fprintf(recievedFile, "%s", buffer); //copy the text from the buffer of the server to the text file on this machine
+        remainingData -= length; //reduce the amount of data left to recieve
         fprintf(stdout, "Recieved ld bytes, only %d left to go\n", length, remainingData);
        }
-       memset(buffer, 0, sizeof(buffer));
-       fclose(recievedFile);
+       memset(buffer, 0, sizeof(buffer)); 
+       fclose(recievedFile); // close the file
        printf("Would you like to recieve more files from the server? Enter 1 for yes, and 0 for no.\n");
-       scanf("%s", continueSize); 
+       scanf("%s", continueSize); //ask for more files
        continueSend = atoi(continueSize); 
-       if (send(clientSocket, continueSize, sizeof(continueSize),0) != -1) {
+       if (send(clientSocket, continueSize, sizeof(continueSize),0) != -1) { //send clients response to server
        printf("Successfully sent continue response to server.\n");
    }
    else {
@@ -93,7 +97,7 @@ int main () {
        exit(EXIT_FAILURE);
    }     
 }
-      close(clientSocket);
+      close(clientSocket); //once the user is done, close the socket
       return 0; 
    
   
--- server.c
+++ server.c
@@ -1,3 +1,8 @@
+
+//server.c: Contains all the code necessary to connect to a client and send it text files located in the same directory as its executable. 
+
+
+
 #include<string.h>
 #include<sys/ioctl.h>
 #include<arpa/inet.h>
@@ -17,12 +22,12 @@ int main() {
    int serverSocket, bindStatus, clientSocket;
    char  fileName[100];
    char  fileSize[256]; 
-   char continueSize[1]; 
+   char continueSize[1]; // string to determine if the user wants more files 
    struct stat fileStatus;
    struct sockaddr_in clientAddress, serverAddress;
    int filePointer;
    socklen_t clientLength = sizeof(clientAddress);
-   serverSocket = socket (PF_INET, SOCK_STREAM, IPPROTO_TCP);
+   serverSocket = socket (PF_INET, SOCK_STREAM, IPPROTO_TCP); //create socket for server
    ssize_t length;
    off_t offset;
    int remainingData; 
@@ -39,9 +44,8 @@ int main() {
    memset(&serverAddress, 0, sizeof(serverAddress));
    serverAddress.sin_family = AF_INET;
    serverAddress.sin_addr.s_addr = INADDR_ANY;
-   serverAddress.sin_port = htons(2500);
-   bindStatus = bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
-   printf("The IP address of the server is: %s\n", inet_ntoa(serverAddress.sin_addr));
+   serverAddress.sin_port = htons(2500); //intialize data for server
+   bindStatus = bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)); //bind the data to the socket
    if (bindStatus == 0) {
 	printf("Bound successfully!\n");
    }
@@ -49,56 +53,56 @@ int main() {
         printf("Bind failure!\n");
         exit(EXIT_FAILURE);
    }
-   if ((listen(serverSocket, 5)) == -1) {
+   if ((listen(serverSocket, 5)) == -1) { //listen for up to 5 incoming connections
         printf("Listen failure!\n");
         exit(EXIT_FAILURE);
    }
-   clientSocket = accept(serverSocket, (struct sockaddr*) &clientAddress, &clientLength); 
+   clientSocket = accept(serverSocket, (struct sockaddr*) &clientAddress, &clientLength); //accept the connections
    if (clientSocket == -1) {
        printf("Could not accept client socket. Please try again\n");
        exit(EXIT_FAILURE);
    }
-   while(continueSend) {
-   if (recv(clientSocket, fileName, sizeof(fileName), 0) != -1) {
+   while(continueSend) { //as long as the user wants more files
+   if (recv(clientSocket, fileName, sizeof(fileName), 0) != -1) { //recieve the filenames the client wants
        printf("Recieved file name\n");
    }
    else {
        printf("File name not recieved properly. Try again\n");
        exit(EXIT_FAILURE);
    }
-     filePointer = open(fileName, O_RDONLY); 
+     filePointer = open(fileName, O_RDONLY); //try and open the file
     if (filePointer == -1) {
         printf("Error opening file.\n");
         exit(EXIT_FAILURE);
     }
-    if (fstat(filePointer, &fileStatus) < 0) {
+    if (fstat(filePointer, &fileStatus) < 0) { //get the files status
         printf("Error fstating\n"); 
         exit(EXIT_FAILURE);
     }
     sprintf(fileSize, "%d", fileStatus.st_size);
-    length = send(clientSocket, fileSize, sizeof(fileSize), 0);
+    length = send(clientSocket, fileSize, sizeof(fileSize), 0); //send the files size to the client
     if (length < 0) {
         printf("Error on sending filesize");
         exit(EXIT_FAILURE);
     }
     printf("The server sent a file size of %d\n", fileStatus.st_size);
     offset = 0;
-    remainingData = fileStatus.st_size;
-    while (((sentData = sendfile(clientSocket, filePointer, &offset, BUFSIZ)) > 0 ) && (remainingData > 0)) {
+    remainingData = fileStatus.st_size; 
+    while (((sentData = sendfile(clientSocket, filePointer, &offset, BUFSIZ)) > 0 ) && (remainingData > 0)) { //while there is still data to be sent, send it to the client
 	 fprintf(stdout, "Server sent %d bytes. Offset = %ld, Remaining Data = %d\n", sentData, offset, remainingData);
-        remainingData -= sentData;
+        remainingData -= sentData; //reduce the offset
          fprintf(stdout, "Server sent %d bytes. Offset = %ld, Remaining Data = %d\n", sentData, offset, remainingData);
     }
-    if (recv(clientSocket, continueSize, sizeof(continueSize), 0)  != -1) {
-        printf("Recieved continue command"); 
+    if (recv(clientSocket, continueSize, sizeof(continueSize), 0)  != -1) { //see if user wants to continue
+        printf("Recieved continue command\n"); 
         continueSend = atoi(continueSize);
     }
     else {
-        printf("Could not recieve continue status. Please try again");
+        printf("Could not recieve continue status. Please try again\n");
         exit(EXIT_FAILURE);
     }
 }
-    close(clientSocket); 
+    close(clientSocket); //once user is done recieving files, close the connection
     close(serverSocket);
     return 0; 
 }
